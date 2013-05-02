80bd90bef648cf183ed0479aa9bc8fea676954ce
Moritz "Gurke" P. <gurkilein@hotmail.de>
2013-05-02 22:53:32 +0200

    Build #2

---
+++ src/de/bplaced/mopfsoft/blocks/Air.java
@@ -0,0 +1,33 @@
+package de.bplaced.mopfsoft.blocks;
+
+import org.newdawn.slick.Color;
+
+import de.bplaced.mopfsoft.items.Item;
+
+public class Air extends Block{
+
+	public Air(int x, int y) {
+		super(x, y);
+	}
+
+	@Override
+	public int getStrength(Item item) {
+		return -1;
+	}
+
+	@Override
+	public int getDrop() {
+		return -1;
+	}
+
+	@Override
+	public Color getColor() {
+		return Color.transparent;
+	}
+
+	@Override
+	public int getId() {
+		return 0;
+	}
+
+}
